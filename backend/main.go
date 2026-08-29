feea9c046cd20a2d48e565c3f1a29cf4328b7178
kenji20thh <amineelayachi322@gmail.com>
2026-08-29 20:39:07 +0100

    first connection to db

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	conn, err := pgx.Connect(context.background(), "postgres://postgres://localhost/ff_fantasy")
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer conn.Close(context.Background())
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "FF Fantasy API")
 	})
