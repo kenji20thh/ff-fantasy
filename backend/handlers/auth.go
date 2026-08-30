625e4b2c9b39893d8ff78175aa2faf509ba61e16
kenji20thh <amineelayachi322@gmail.com>
2026-08-30 12:21:20 +0100

    request struct

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -1,6 +1,10 @@
 package handlers
 
-import "github.com/jackc/pgx/v5"
+import (
+	"net/http"
+
+	"github.com/jackc/pgx/v5"
+)
 
 type AuthHandler struct {
 	DB *pgx.Conn
@@ -11,4 +15,11 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	
+
+	var request struct {
+		Username string `json:"username"`
+		EMail    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+}
