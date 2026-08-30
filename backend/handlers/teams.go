a53e975fe823083ff04b3138cd8bf29557634a34
kenji20thh <amineelayachi322@gmail.com>
2026-08-30 10:50:45 +0100

    teams refactor

--- backend/handlers/teams.go
+++ backend/handlers/teams.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"net/http"
+)
+
+type teamHandler struct {
+	DB *pgx.Conn
+}
+func (h *teamHandler) GetTeams() (w, http.ResponseWriter, r *http.Request) {
+	rows, err := h.DB;Query(
+		context.Background(),
+		"SELECT id, name FROM teams"
+	)
+}
