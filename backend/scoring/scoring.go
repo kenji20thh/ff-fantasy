4df8782671091057a41b4ed302a85506eb128062
kenji20thh <amineelayachi322@gmail.com>
2026-08-30 17:34:55 +0100

    placement points calculation

---
+++ backend/scoring/scoring.go
@@ -0,0 +1,28 @@
+package scoring
+
+func PlacementPoints(placement int) int {
+	switch placement {
+	case 1:
+		return 15
+	case 2:
+		return 12
+	case 3:
+		return 10
+	case 4:
+		return 8
+	case 5:
+		return 6
+	case 6:
+		return 5
+	case 7:
+		return 4
+	case 8:
+		return 3
+	case 9:
+		return 2
+	case 10:
+		return 1
+	default:
+		return 0
+	}
+}
