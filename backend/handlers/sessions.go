ab57ce4a3aa6163f8d6ae48e5549026ac476ab04
kenji20thh <amineelayachi322@gmail.com>
2026-08-30 15:02:55 +0100

    create session

--- backend/handlers/sessions.go
+++ backend/handlers/sessions.go
@@ -1,8 +1,36 @@
 package handlers
 
-import "sync"
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"sync"
+)
 
 type sessionStore struct {
 	sessions map[string]int // sessionID -> userID
 	mu       sync.RWMutex
+}
+
+func newSessionStore() *sessionStore {
+	return &sessionStore{
+		sessions: make(map[string]int),
+	}
+}
+
+func (s *sessionStore) Create(userID int) (string, error) {
+	bytes := make([]byte, 32)
+
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	sessionID := hex.EncodeToString(bytes)
+
+	s.mu.Lock()
+	s.sessions[sessionID] = userID
+	s.mu.Unlock()
+
+	return sessionID, nil
+
 }
