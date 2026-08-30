f8484d6da7945cd81d3f68438ae3dcf85d51f77a
kenji20thh <amineelayachi322@gmail.com>
2026-08-30 10:31:20 +0100

    refactor db

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,11 +1,26 @@
 package database
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
 func Connect() (*pgx.Conn, error) {
 	conn, err := pgx.Connect(
 		context.Background(),
 		"postgres://kenji20th:MrR@b@!@localhost:5432/ff_fantasy",
 	)
+
 	if err != nil {
 		return nil, err
 	}
-	
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		conn.Close(context.Background())
+		return nil, err
+	}
+
+	return conn, nil
+}
