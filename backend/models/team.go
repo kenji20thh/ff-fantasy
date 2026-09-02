c84da11597af329baf3dadb7a24b8d2243bbaff8
kenji20thh <amineelayachi322@gmail.com>
2026-09-02 19:45:56 +0100

    update team json to include logo

--- backend/models/team.go
+++ backend/models/team.go
@@ -1,6 +1,7 @@
 package models
 
 type Team struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	LogoURL string `json:"logo_url"`
 }
