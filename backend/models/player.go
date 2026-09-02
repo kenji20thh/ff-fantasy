958b99af79319a7ad72d404f0ecfb42680c6c5f6
kenji20thh <amineelayachi322@gmail.com>
2026-09-02 19:46:16 +0100

    update player json to include picture

--- backend/models/player.go
+++ backend/models/player.go
@@ -1,7 +1,8 @@
 package models
 
 type Player struct {
-	ID       int    `json:"id"`
-	TeamID   int    `json:"team_id"`
-	Nickname string `json:"nickname"`
+	ID         int    `json:"id"`
+	TeamID     int    `json:"team_id"`
+	Nickname   string `json:"nickname"`
+	PictureURL string `json:"picture_url"`
 }
