7aa3191e0468385013f194cccfcf33219985fc8d
kenji20thh <amineelayachi322@gmail.com>
2026-09-03 19:13:37 +0100

    fix production CORS

--- backend/main.go
+++ backend/main.go
@@ -13,7 +13,7 @@ import (
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://ff-fantasy.vercel.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set(
 			"Access-Control-Allow-Methods",
